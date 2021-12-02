2b46e2696718a778fb6309bc65e285ec5da8a586
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2021-12-02 14:45:58 +0300

    Convert Icons into JSX & Add Assets

---
+++ src/lib/icons/Ethereum.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function EthereumIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      version="1.1"
+      viewBox="0 0 1920 1920"
+      xmlSpace="preserve"
+      {...props}
+    >
+      <path fill="#8A92B2" d="M959.8 80.7L420.1 976.3 959.8 731z"></path>
+      <path fill="#62688F" d="M959.8 731L420.1 976.3 959.8 1295.4z"></path>
+      <path fill="#62688F" d="M1499.6 976.3L959.8 80.7 959.8 731z"></path>
+      <path fill="#454A75" d="M959.8 1295.4L1499.6 976.3 959.8 731z"></path>
+      <path fill="#8A92B2" d="M420.1 1078.7L959.8 1839.3 959.8 1397.6z"></path>
+      <path fill="#62688F" d="M959.8 1397.6L959.8 1839.3 1499.9 1078.7z"></path>
+    </svg>
+  );
+}
+
+export default EthereumIcon;
