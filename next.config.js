2994408b15ef9a65de174bb668c73b154e161149
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2021-12-06 00:15:01 +0300

    Disable Page Extensions for Vercel SWC

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: true,
   i18n,
-  pageExtensions: [
-    "page.mdx",
-    "page.md",
-    "page.jsx",
-    "page.js",
-    "page.tsx",
-    "page.ts",
-  ], //for custom page extensions
   images: {
     domains: ["upload.wikimedia.org"],
   },
