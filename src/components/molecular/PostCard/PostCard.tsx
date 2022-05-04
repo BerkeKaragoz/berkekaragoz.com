44b82eb9dd5a7d1437deb83ab2379ff5b043f662
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-04 17:48:48 +0300

    refactor(PostList): split PostCard as a component

---
+++ src/components/molecular/PostCard/PostCard.tsx
@@ -0,0 +1,53 @@
+import LinkText from "@/components/atomic/LinkText/LinkText"
+import { PostMeta } from "@/lib/api/blog"
+import { COMMON_TNS } from "@/lib/i18n/consts"
+import { ComponentPropsWithActiveTranslation } from "@/lib/types/i18n"
+import { estimateReadingMinutes } from "@/lib/utils"
+import { DEFAULT_LOCALE } from "@/lib/utils/consts"
+import React from "react"
+import { useTranslation, withTranslation } from "react-i18next"
+
+type PostCardProps = ComponentPropsWithActiveTranslation<{
+   postMeta: PostMeta
+}>
+
+export const PostCard: React.FC<PostCardProps> = (props) => {
+   const { postMeta, i18n } = props
+   const locale = i18n.language ?? DEFAULT_LOCALE
+
+   const { t: ct } = useTranslation([COMMON_TNS])
+
+   return (
+      <li className="flex flex-col justify-between p-2 card">
+         <div>
+            <LinkText href={`/p/${postMeta.slug}`} className="me-2 h3">
+               {postMeta.title}
+            </LinkText>
+            <div className="inline-block">
+               <span className="text-subtitle-color">
+                  {new Date(postMeta.date).toLocaleDateString(locale)}
+               </span>
+               <span className="inline-block ml-1 opacity-50 text-subtitle-color">
+                  {`• ${estimateReadingMinutes(postMeta.wordCount)} ${ct(
+                     "min read"
+                  )}`}
+               </span>
+               <span className="hidden ml-2 text-sm sm:inline-block text-subtitle-color opacity-30">
+                  {`/p/${postMeta.slug}`}
+               </span>
+            </div>
+         </div>
+         <p className="my-1 text-subtitle-color max-w-prose">{postMeta.excerpt}</p>
+         <p>
+            {postMeta.tags.map((tag, i) => (
+               <span key={`${tag}-${postMeta.slug}`}>
+                  <LinkText href={`/posts/${tag}`}>{`#${tag}`}</LinkText>
+                  {i !== postMeta.tags.length - 1 && `, `}
+               </span>
+            ))}
+         </p>
+      </li>
+   )
+}
+
+export default withTranslation(COMMON_TNS)(PostCard)
